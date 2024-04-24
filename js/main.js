1fc3bf58b95d1bedae585f8801103d52fa461844
Egor3228 <egorkogutstudies@mail.ru>
2024-04-24 12:53:36 +0700

    end 4 work

--- js/main.js
+++ js/main.js
@@ -16,7 +16,11 @@ let app = new Vue({
             variantId: 2235,
             variantColor: 'blue'
         }
-     ]
+        ]
+        ,
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+     
     }
  })
+ 
  
