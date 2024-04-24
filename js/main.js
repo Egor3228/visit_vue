17fc5b711774634da22ac52834b111e51ba68979
Egor3228 <egorkogutstudies@mail.ru>
2024-04-24 13:30:08 +0700

    end 5 work

--- js/main.js
+++ js/main.js
@@ -1,26 +1,36 @@
 let app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
-        altText: "A pair of socks",
-        inStock: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-    
-        variants: [
+  el: '#app',
+  data: {
+
+      product: "Socks",
+      image: "./assets/vmSocks-green-onWhite.jpg",
+      altText: "A pair of socks",
+      inStock: true,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
         {
             variantId: 2234,
-            variantColor: 'green'
+            variantColor: 'green',
+            variantImage: "./assets/vmSocks-green-onWhite.jpg",
         },
         {
             variantId: 2235,
-            variantColor: 'blue'
+            variantColor: 'blue',
+            variantImage: "./assets/vmSocks-blue-onWhite.jpg",
         }
-        ]
-        ,
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-     
+     ],
+    
+    cart: 0, 
+
+methods: {
+    addToCart() {
+        this.cart += 1
+    },
+    deleteFromCart() {
+        this.cart -= 1
+    },
+    updateProduct(variantImage) {
+        this.image = variantImage
     }
- })
- 
- 
+}
+}})
